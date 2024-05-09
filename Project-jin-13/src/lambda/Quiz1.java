a7cd74dbbae524b454dedebca9aaf5931bc57dee
jinjnnho <junho11_kr@naver.com>
2024-05-09 13:34:36 +0900

    24.05.09

--- Project-jin-13/src/lambda/Quiz1.java
+++ Project-jin-13/src/lambda/Quiz1.java
@@ -10,12 +10,19 @@ public class Quiz1 {
 		
 		// 람다식 함수 출력결과
 		Mynumber num2 = (x, y) -> x > y ? x : y;
-		System.out.println(num2.max(3, 5));
+		System.out.println(num2.max(10, 5));
 		
 		// 익명 함수 출력결과
+		Mynumber num3 = new Mynumber(){
+			@Override
+			public int max(int x, int y) {
+				return (x > y) ? x : y;
+			}
+		};
+		System.out.println(num3.max(3,5));
+		System.out.println(num3.max(15,5));
 		
 	}
-
 }
 
 // 함수형 인터페이스
