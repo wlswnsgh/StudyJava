94d255eb0f7d4ec305a146acf8a377a36f8d461b
jinjnnho <junho11_kr@naver.com>
2024-05-20 09:54:17 +0900

    24.05.20

--- project-jin-15/src/main/Quiz8.java
+++ project-jin-15/src/main/Quiz8.java
@@ -1,10 +1,36 @@
 package main;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quiz8 {
 
 	public static void main(String[] args) {
 		
+		List<String> fileLst = new ArrayList<String>();
+		scanDir("C:\\q", fileLst);
+		
+		for(String fullPath: fileLst) {
+			System.out.println(fullPath);
+		} 
 		
 	}
+	
 
+	public static void scanDir(String folderPath, List<String> fileLst) {
+		
+		File[] files = new File(folderPath).listFiles();
+		
+		for(File f : files) {
+			fileLst.add(f.getPath());
+			
+			if(f.isDirectory()) {
+				scanDir(f.getPath(), fileLst);
+			} 
+		}
+		
+	}
+	
+	
 }
--- project-jin-15/src/main/Quiz9.java
+++ project-jin-15/src/main/Quiz9.java
@@ -10,15 +10,15 @@ public class Quiz9 {
 	public static void main(String[] args) {
 		
 		try {
-			FileWriter Q1 = new FileWriter("quiz9.txt"); // quiz9 파일 생성
+			FileWriter Q9 = new FileWriter("quiz9.txt"); // quiz9 파일 생성
 			
 			File file = new File("C:\\풀스택"); // 풀스텍 파일 경로
-			File[] list = file.listFiles(); // .listFiles()로 풀스텍 파일 안에 있는 파일들을 리스트로 가져오기
+			File[] list1 = file.listFiles(); // .listFiles()로 풀스텍 파일 안에 있는 파일들을 리스트로 가져오기
 			
-			for(int i = 0; i < list.length; i++) {
-				Q1.write(list[i].getName() + "\n"); // 풀스텍 파일 목록 출력
-			} Q1.flush();			
-			
+			for(int i = 0; i < list1.length; i++) {
+				 Q9.write(list1[i].getName() + "\n"); // 풀스텍 파일 목록 출력
+			} Q9.flush();		
+				
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
